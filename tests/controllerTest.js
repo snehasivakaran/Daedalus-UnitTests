c8f9df70026fe5f3674894f8e49be24976fc0e5d
snehasivakaran <sinegaask@icloud.com>
2025-11-04 17:18:09 +0100

    Add files via upload

---
+++ tests/controllerTest.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const apiTemperature = require('../public/js/auxiliaryAPI')
+
+//TODO Unit Tests to implement
+describe("fahrenheitToCelsius()", function(){
+
+
+});
